5b69749bbe9eb8d7b806d61ee08e6bd2e9ab9157
rolandzhao <rolandzhao@foxmail.com>
2016-03-27 23:39:37 +0800

    1

---
+++ roland01-accumulator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  roland01-accumulator
+//
+//  Created by roland on 16/3/25.
+//  Copyright © 2016年 roland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ roland01-accumulator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  roland01-accumulator
+//
+//  Created by roland on 16/3/25.
+//  Copyright © 2016年 roland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
